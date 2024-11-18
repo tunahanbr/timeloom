7bd9de0f676ac1d328a59e07a211b919f5b6f12d
Tunahan <47463551+tunahanbr@users.noreply.github.com>
2024-11-18 16:31:47 +0100

    added some hover animation on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,14 +5,14 @@ function Sidebar({projects}) {
     <>
       {/* Sidebar with projects */}
       <div className="mx-2">
-        <div className="bg-gray-50 rounded-xl w-20 h-80 my-auto overflow-y-auto scrollbar-hide shadow-sm">
+        <div className="bg-gray-50 rounded-xl w-16 h-80 my-auto overflow-y-auto scrollbar-hide shadow-sm">
           <div className="flex flex-col items-center my-6 gap-1">
             {projects.map(project => (
-              <div className="w-12 h-12 rounded-lg bg-white">
+              <div className="w-12 h-12 rounded-lg bg-white hover:scale-110 transition-transform cursor-pointer">
                 <img className="p-1" src={project.icon} />
               </div>
             ))}
-          <button className="w-12 h-12 text-black border-dashed border-2 rounded-lg border-black">+</button>
+            <button className="w-12 h-12 text-black border-dashed border-2 rounded-lg border-black hover:scale-110 transition-transform">+</button>
           </div>
         </div>
       </div>
