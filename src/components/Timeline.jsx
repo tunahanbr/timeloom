38784357f117ca0eec65296f79be94768b5d7c04
Tunahan <47463551+tunahanbr@users.noreply.github.com>
2024-11-18 21:43:21 +0100

    text wrapping, when name is exceeded

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -6,36 +6,35 @@ function Timeline({ timeline }) {
   const [currentPage, setCurrentPage] = useState(1);
   // Number of items per page (can be adjusted)
   const itemsPerPage = 6;
-
   // Calculate the items to display based on currentPage and itemsPerPage
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = timeline.slice(indexOfFirstItem, indexOfLastItem);
-
   // Calculate total pages
   const totalPages = Math.ceil(timeline.length / itemsPerPage);
-
   // Function to handle page change
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <>
-      <div className="bg-gray-50 overflow-x-auto shadow-sm sm:rounded-lg">
+      <div className="bg-gray-50 shadow-sm sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500">
           <thead className="text-xs text-gray-700 uppercase bg-gray-100">
             <tr>
-              <td className="px-6 py-3">Name</td>
-              <td className="px-6 py-3">Start</td>
-              <td className="px-6 py-3">Duration</td>
-              <td></td>
+              <td className="px-6 py-3 w-1/2">Name</td>
+              <td className="px-6 py-3 w-1/4">Start</td>
+              <td className="px-6 py-3 w-1/4">Duration</td>
+              <td className="w-10"></td>
             </tr>
           </thead>
           <tbody>
             {currentItems.map((item, i) => (
               <tr key={i}>
-                <td className="px-6 py-3">{item.name}</td>
-                <td className="px-6 py-3">{item.start_time}</td>
-                <td className="px-6 py-3">{item.duration}</td>
+                <td className="px-6 py-3 break-words">
+                  <div className="max-w-xs">{item.name}</div>
+                </td>
+                <td className="px-6 py-3 whitespace-nowrap">{item.start_time}</td>
+                <td className="px-6 py-3 whitespace-nowrap">{item.duration}</td>
                 <td className="px-4 py-2 hover:text-black cursor-pointer">
                   <RiMore2Line />
                 </td>
@@ -44,7 +43,6 @@ function Timeline({ timeline }) {
           </tbody>
         </table>
       </div>
-
       {/* Pagination controls */}
       <div className="flex justify-center mt-4">
         <button
